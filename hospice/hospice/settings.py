e3cab8db314504e215c9bb2e4e59b10cd82a7960
wasim619 <30339394+wasim619@users.noreply.github.com>
2020-04-30 12:53:51 +0530

    Update settings.py

--- hospice/hospice/settings.py
+++ hospice/hospice/settings.py
@@ -130,6 +130,6 @@ STATIC_ROOT = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,"static"),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
